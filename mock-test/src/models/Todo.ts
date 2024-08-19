7c4f8146efe240336ff17b6be7fca6717dbd7c1f
CarrotEasy0214 <seongmin0214@kakao.com>
2024-08-19 09:55:19 +0900

    edit:actionCheck

---
+++ mock-test/src/models/Todo.ts
@@ -0,0 +1,23 @@
+import { Model } from "sequelize-typescript";
+import { AutoIncrement, Column, DataType, PrimaryKey, Table } from "sequelize-typescript";
+
+@Table({
+  tableName: "todos",
+  underscored: true,
+})
+export default class Todo extends Model {
+  @PrimaryKey
+  @AutoIncrement
+  @Column(DataType.INTEGER.UNSIGNED)
+  id!: number;
+
+  @Column(DataType.STRING(255))
+  title!: string;
+
+  @Column({
+    type: DataType.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+  })
+  isCompleted!: boolean;
+}
